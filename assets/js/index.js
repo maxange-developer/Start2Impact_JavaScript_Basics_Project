3358b6b9b3a5010f861ca76ed7b07f355332a50c
maxange-developer <massiangelone73@gmail.com>
2023-06-15 12:34:08 +0200

    Fine Codice JS

---
+++ assets/js/index.js
@@ -0,0 +1,72 @@
+const body = document.body;
+
+// Creazione Section
+
+const section = document.createElement('section');
+body.appendChild(section);
+
+// Funzione di creazione elementi HTML
+
+function create(nome, tag, parent, id, text, classSelector) {
+    nome = document.createElement(tag);
+    parent.appendChild(nome);
+    nome.setAttribute('id', `${id}`);
+    nome.textContent = `${text}`;
+    if (classSelector) {
+        nome.setAttribute('class', `${classSelector}`)
+    }
+    return nome;
+}
+
+// Container Principale
+const container = create('container', 'div', section, 'container', '');
+
+// Titolo
+const title = create('header', 'h1', container, '', 'JS Counter', '');
+
+// Contenitore Valore Counter
+const valueContainer = create('value-container', 'div', container, 'value-container', '');
+
+// Valore Counter
+const counter = create('value', 'p', valueContainer, 'value', '0', '');
+
+// Container Pulsanti
+const btnContainer = create('btnContainer', 'div', container, 'btn-container', '');
+
+// Pulsante -
+const btnDecrement = create('btnDecrement', 'div', btnContainer, 'decrement','', 'btn');
+const valueBtnDecrement = create ('valueBtnDecrement', 'p', btnDecrement, 'value-btn-decrement', '-');
+
+// Pulsante Reset
+const btnReset = create('btnReset', 'div', btnContainer, 'reset', '', 'btn');
+const valueBtn = create('valueBtn', 'p', btnReset, 'btn-reset', 'Reset');
+
+// Pulsante +
+const btnIncrement = create('btnIncrement', 'div', btnContainer, 'increment', '', 'btn');
+const valueBtnIncrement = create('valueBtnIncrement', 'p', btnIncrement, 'value-btn-increment', '+');
+
+
+
+
+const buttons = document.querySelectorAll('.btn');
+const buttonsContainer = document.querySelector('#btn-container');
+const value = document.querySelector('#value')
+
+let count = 0;
+
+buttonsContainer.addEventListener('click', function (e){
+    const styles = e.target.id;
+    if (counter <= 0) {
+        count = 0
+    }
+    else if (styles.includes('decrement') && count > 0 ){
+        count--;
+    }
+    else if (styles.includes('increment')) {
+        count++;
+    }
+    else if (styles.includes('reset')) {
+        count = 0
+    }
+    value.textContent = count;
+})
